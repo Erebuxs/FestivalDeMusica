f6347470ccd935f8e7647d70bfe8e997d0293a02
Daniel Amores <carlos.amores19@outlook.com>
2025-03-04 12:30:33 -0600

    generando galeria con js

--- build/js/app.js
+++ build/js/app.js
@@ -1,2 +1,17 @@
-alert('h0laaa')
+document.addEventListener('DOMContentLoaded', function() {
+    crearGaleria()
+})
+
+function crearGaleria() {
+    const galeria = document.querySelector('.galeria-imagenes')
+
+    for(let i = 1; i <= 16; i++){
+        const imagen = document.createElement('IMG')
+        imagen.src = `src/img/gallery/full/${i}.jpg`
+        imagen.alt = ('Imagen Galeria')
+        console.log(imagen)
+
+        galeria.appendChild(imagen)
+    }
+}
 
